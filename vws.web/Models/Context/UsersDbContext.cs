9869a83eb6d49c093acabed608b427384c1c928c
AmirHosseinMissMe <amirhossein2324236@gmail.com>
2020-12-14 22:11:57 +0330

    Added sql server service and connection string.

---
+++ vws.web/Models/Context/UsersDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace vws.web.Models.Context
+{
+    public class UsersDbContext : DbContext
+    {
+    }
+}
--- vws.web/Startup.cs
+++ vws.web/Startup.cs
@@ -2,9 +2,11 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.SpaServices.AngularCli;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using vws.web.Models.Context;
 
 namespace vws.web
 {
@@ -21,6 +23,10 @@ namespace vws.web
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            services.AddDbContextPool<UsersDbContext>(options =>
+            {
+                options.UseSqlServer(Configuration.GetConnectionString("SqlServer"));
+            });
             // In production, the Angular files will be served from this directory
             services.AddSpaStaticFiles(configuration =>
             {
