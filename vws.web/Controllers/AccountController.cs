8e9b26fd7bfba803df50f92de1d309b06f456e50
AmirHosseinMissMe <amirhossein2324236@gmail.com>
2020-12-15 13:42:51 +0330

    Added register model in account controller.

---
+++ vws.web/Controllers/AccountController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using vws.web.Models;
+
+namespace vws.web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+        private readonly UserManager<IdentityUser> userManager;
+        private readonly RoleManager<IdentityRole> roleManager;
+        private readonly SignInManager<IdentityUser> signInManager;
+        private readonly IConfiguration configuration;
+
+        public AccountController(UserManager<IdentityUser> _userManager, RoleManager<IdentityRole> _roleManager,
+            SignInManager<IdentityUser> _signInManager, IConfiguration _configuration)
+        {
+            userManager = _userManager;
+            roleManager = _roleManager;
+            signInManager = _signInManager;
+            configuration = _configuration;
+        }
+
+        [HttpPost]
+        [Route("register")]
+        public async Task<IActionResult> Register([FromBody] RegisterModel model)
+        {
+            var userExists = await userManager.FindByNameAsync(model.Username);
+            if(userExists != null)
+                return StatusCode(StatusCodes.Status500InternalServerError,
+                    new ResponseModel { Status = "Error", Message = "User already exists!" });
+
+            IdentityUser user = new IdentityUser()
+            {
+                Email = model.Email,
+                SecurityStamp = Guid.NewGuid().ToString(),
+                UserName = model.Username
+            };
+            var result = await userManager.CreateAsync(user, model.Password);
+            if (!result.Succeeded)
+                return StatusCode(StatusCodes.Status500InternalServerError, new ResponseModel { Status = "Error", Message = "User creation failed!" });
+            return Ok(new ResponseModel { Status = "Success", Message = "User created successfully!" });
+        }
+    }
+}
