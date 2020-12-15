f27672abbe68c63dfdca0cde965e236d30398d10
AmirHosseinMissMe <amirhossein2324236@gmail.com>
2020-12-15 14:58:33 +0330

    Added swagger.

--- vws.web/Startup.cs
+++ vws.web/Startup.cs
@@ -7,6 +7,8 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.Swagger;
 using vws.web.Models.Context;
 
 namespace vws.web
@@ -24,10 +26,22 @@ namespace vws.web
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Version = "v1",
+                    Title = "VWS API",
+                    Description = "VWS ASP.NET Core Web API"
+                });
+            });
+            
             services.AddDbContextPool<UsersDbContext>(options =>
             {
                 options.UseSqlServer(Configuration.GetConnectionString("SqlServer"));
             });
+
             services.AddIdentity<IdentityUser, IdentityRole>()
                     .AddEntityFrameworkStores<UsersDbContext>()
                     .AddDefaultTokenProviders();
@@ -51,6 +65,13 @@ namespace vws.web
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "VWSAPI");
+                c.RoutePrefix = string.Empty;
+            });
 
             app.UseHttpsRedirection();
             app.UseStaticFiles();
