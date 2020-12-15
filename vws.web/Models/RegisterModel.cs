00ce48f54f6ed5f6b020f76abab3a9d047b4444b
AmirHosseinMissMe <amirhossein2324236@gmail.com>
2020-12-15 12:39:17 +0330

    Added register model.

---
+++ vws.web/Models/RegisterModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace vws.web.Models
+{
+    public class RegisterModel
+    {
+        [Required(ErrorMessage = "نام کاربری الزامی است")]
+        public string Username { get; set; }
+        [EmailAddress]
+        [Required(ErrorMessage = "ایمیل الزامی است")]
+        public string Email { get; set; }
+        [Required(ErrorMessage = "رمز عبور الزامی است")]
+        public string Password { get; set; }
+    }
+}
