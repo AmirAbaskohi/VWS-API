670e590fb03b9e9e29633df4247a49696934da6d
AmirHosseinMissMe <amirhossein2324236@gmail.com>
2020-12-17 18:48:53 +0330

    Added login controller.

--- vws.web/Controllers/AccountController.cs
+++ vws.web/Controllers/AccountController.cs
@@ -1,11 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
 using vws.web.Models;
 
 namespace vws.web.Controllers
@@ -33,7 +36,7 @@ namespace vws.web.Controllers
         public async Task<IActionResult> Register([FromBody] RegisterModel model)
         {
             var userExists = await userManager.FindByNameAsync(model.Username);
-            if(userExists != null)
+            if (userExists != null)
                 return StatusCode(StatusCodes.Status500InternalServerError,
                     new ResponseModel { Status = "Error", Message = "User already exists!" });
 
@@ -47,6 +50,44 @@ namespace vws.web.Controllers
             if (!result.Succeeded)
                 return StatusCode(StatusCodes.Status500InternalServerError, new ResponseModel { Status = "Error", Message = "User creation failed!" });
             return Ok(new ResponseModel { Status = "Success", Message = "User created successfully!" });
+        }
+
+        [HttpPost]
+        [Route("login")]
+        public async Task<IActionResult> Login([FromBody] LoginModel model)
+        {
+            if (signInManager.IsSignedIn(User))
+                return StatusCode(StatusCodes.Status500InternalServerError, new ResponseModel { Status = "Error", Message = "User already signed in!" });
+
+            bool rememberMe = true;
+            if (model.RememberMe.HasValue)
+                rememberMe = model.RememberMe.Value;
+
+            bool isEmail = false;
+            if (model.UsernameOrEmail.Contains("@"))
+                isEmail = true;
+
+            var user = isEmail ? await userManager.FindByEmailAsync(model.UsernameOrEmail) :
+                await userManager.FindByNameAsync(model.UsernameOrEmail);
+
+            if (user != null && await userManager.CheckPasswordAsync(user, model.Password))
+            {
+                var authSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["JWT:Secret"]));
+
+                var token = new JwtSecurityToken(
+                    issuer: configuration["JWT:Issuer"],
+                    audience: configuration["JWT:Audience"],
+                    expires: DateTime.Now.AddMinutes(2),
+                    signingCredentials: new SigningCredentials(authSigningKey, SecurityAlgorithms.HmacSha256)
+                    );
+
+                return Ok(new
+                {
+                    token = new JwtSecurityTokenHandler().WriteToken(token),
+                    expiration = token.ValidTo
+                });
+            }
+            return Unauthorized();
         }
     }
 }
