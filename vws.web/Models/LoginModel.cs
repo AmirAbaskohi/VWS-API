f1aa63ef7166477d9c938fe1bf104f97f5f6bc99
AmirHosseinMissMe <amirhossein2324236@gmail.com>
2020-12-17 16:04:07 +0330

    Added login model.

---
+++ vws.web/Models/LoginModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace vws.web.Models
+{
+    public class LoginModel
+    {
+        [Required(ErrorMessage = "ایمیل یا نام کاربری الزامی است")]
+        public string UsernameOrEmail { get; set; }
+        [Required(ErrorMessage = "رمز عبور الزامی است")]
+        public string Password { get; set; }
+    }
+}
