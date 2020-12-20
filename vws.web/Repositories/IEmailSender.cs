ae5fd90b38162c0dc78d6c944a3c1c5495d79d39
AmirHosseinMissMe <amirhossein2324236@gmail.com>
2020-12-20 13:43:20 +0330

    Added email sender repository.

---
+++ vws.web/Repositories/IEmailSender.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace vws.web.Repositories
+{
+    public interface IEmailSender
+    {
+        public Task SendEmailAsync(string toEmail, string subject, string message, bool isMessageHtml = false);
+    }
+}
