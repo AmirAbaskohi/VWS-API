a97234ae034eafc7b0e8a78d5005c1ecb80ebfe8
AmirHosseinMissMe <amirhossein2324236@gmail.com>
2020-12-21 21:20:28 +0330

    Added reset password model.

---
+++ vws.web/Models/ResetPasswordModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace vws.web.Models
+{
+    public class ResetPasswordModel
+    {
+        [Required]
+        public string Email { get; set; }
+        [Required]
+        public string ValidationCode { get; set; }
+        [Required]
+        public string NewPassword { get; set; }
+    }
+}
