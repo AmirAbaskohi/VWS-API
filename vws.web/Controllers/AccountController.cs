7573e36b1ba72b8c5dd69337effe253e217aa58d
AmirHosseinMissMe <amirhossein2324236@gmail.com>
2020-12-21 20:14:11 +0330

    Updated send confirmation email action method.

--- vws.web/Controllers/AccountController.cs
+++ vws.web/Controllers/AccountController.cs
@@ -127,7 +127,7 @@ namespace vws.web.Controllers
         public async Task<IActionResult> SendConfirmEmail([FromBody] UserModel model)
         {
 
-            var user = await userManager.FindByNameAsync(model.UserName);
+            var user = await userManager.FindByEmailAsync(model.Email);
             if(user == null)
                 return StatusCode(StatusCodes.Status500InternalServerError, new ResponseModel { Status = "Error", Message = "User does not exist!" });
 
