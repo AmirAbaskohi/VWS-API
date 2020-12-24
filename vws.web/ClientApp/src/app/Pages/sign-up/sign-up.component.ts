c0974c7f907e224be99f4e1186f168ad2bb76f7e
Mohammad Sadra <mohammadsadrahaeri@gmail.com>
2020-12-24 18:21:00 +0330

    Added form group to sign-up.component.ts Added images to assets

--- vws.web/ClientApp/src/app/Pages/sign-up/sign-up.component.ts
+++ vws.web/ClientApp/src/app/Pages/sign-up/sign-up.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-sign-up',
@@ -9,9 +10,24 @@ export class SignUpComponent implements OnInit {
 
   hide = true;
   logo = '/assets/Images/logo.png';
-  constructor() { }
+
+  signUpForm: FormGroup;
+
+  constructor(private _formBuilder: FormBuilder) {
+  }
 
   ngOnInit() {
+    this.signUpForm = this._formBuilder.group({
+      username: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(5)]]
+    });
+  }
+
+  clicked(): void {
+    console.log(this.signUpForm.get('username').value);
+    console.log(this.signUpForm.get('email').value);
+    console.log(this.signUpForm.get('password').value);
   }
 
 }
