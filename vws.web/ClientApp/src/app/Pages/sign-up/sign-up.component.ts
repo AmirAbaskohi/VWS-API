60eab12f4034a5ffad68df299aa2d57dee175d66
Mohammad Sadra <mohammadsadrahaeri@gmail.com>
2020-12-24 16:52:24 +0330

    Added sign-up component

---
+++ vws.web/ClientApp/src/app/Pages/sign-up/sign-up.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-sign-up',
+  templateUrl: './sign-up.component.html',
+  styleUrls: ['./sign-up.component.scss']
+})
+export class SignUpComponent implements OnInit {
+
+  hide = true;
+  logo = '/assets/Images/logo.png';
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
