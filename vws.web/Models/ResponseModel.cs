edcc13442b5f515d3992f4b4b0ae07517f9892da
AmirHosseinMissMe <amirhossein2324236@gmail.com>
2020-12-28 13:35:34 +0330

    Updated response model to return errors.

--- vws.web/Models/ResponseModel.cs
+++ vws.web/Models/ResponseModel.cs
@@ -7,7 +7,14 @@ namespace vws.web.Models
 {
     public class ResponseModel
     {
+        public ResponseModel()
+        {
+            Errors = new List<string>();
+        }
+        public void AddError(string error) => Errors.Add(error);
         public string Status { get; set; }
         public string Message { get; set; }
+        public bool HasError { get; set; }
+        public List<string> Errors { get; set; }
     }
 }
