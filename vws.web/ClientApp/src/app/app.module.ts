deb116181e8ad510d2dba5b9b5d37d51ceaaccfb
S.Alireza Ezaz <S.Alireza Ezaz@SALIREZA-EZAZ>
2020-12-29 18:32:38 +0330

    ctrl alt L :)

--- vws.web/ClientApp/src/app/app.module.ts
+++ vws.web/ClientApp/src/app/app.module.ts
@@ -1,12 +1,12 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
-import { RouterModule } from '@angular/router';
-import { AppComponent } from './app.component';
-import { HomeComponent } from './Home/home.component';
-import { SignInComponent } from './Pages/sign-in/sign-in.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {HttpClientModule, HTTP_INTERCEPTORS} from '@angular/common/http';
+import {RouterModule} from '@angular/router';
+import {AppComponent} from './app.component';
+import {HomeComponent} from './Home/home.component';
+import {SignInComponent} from './Pages/sign-in/sign-in.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {MatStepperModule} from '@angular/material/stepper';
 import {ReactiveFormsModule} from '@angular/forms';
 import {MatFormFieldModule} from '@angular/material/form-field';
@@ -22,10 +22,10 @@ import {MatCardModule} from '@angular/material/card';
 import {MatAutocompleteModule} from '@angular/material/autocomplete';
 import {MatCheckboxModule} from '@angular/material/checkbox';
 import {MatRadioModule} from '@angular/material/radio';
-import { OnBoardingComponent } from './Pages/on-boarding/on-boarding.component';
-import { SignUpComponent } from './Pages/sign-up/sign-up.component';
-import { ForgetPasswordComponent } from './Pages/forget-password/forget-password.component';
-import { ProfileSettingsComponent } from './Pages/profile-settings/profile-settings.component';
+import {OnBoardingComponent} from './Pages/on-boarding/on-boarding.component';
+import {SignUpComponent} from './Pages/sign-up/sign-up.component';
+import {ForgetPasswordComponent} from './Pages/forget-password/forget-password.component';
+import {ProfileSettingsComponent} from './Pages/profile-settings/profile-settings.component';
 import {VwsInterceptor} from './Utilities/VwsInterceptor';
 import {SignInService} from "./Services/sign-in.service";
 import {SignUpService} from "./Services/sign-up.service";
@@ -41,36 +41,37 @@ import {SignUpService} from "./Services/sign-up.service";
     ForgetPasswordComponent,
     ProfileSettingsComponent
   ],
-    imports: [
-        BrowserModule.withServerTransition({appId: 'ng-cli-universal'}),
-        HttpClientModule,
-        FormsModule,
-        RouterModule.forRoot([
-            {path: '', component: HomeComponent, pathMatch: 'full'},
-            {path: 'sign-in', component: SignInComponent, pathMatch: 'full'},
-            {path: 'on-boarding', component: OnBoardingComponent, pathMatch: 'full'},
-            {path: 'sign-up', component: SignUpComponent, pathMatch: 'full'},
-            {path: 'forget-password', component: ForgetPasswordComponent, pathMatch: 'full'},
-            {path: 'profile-settings', component: ProfileSettingsComponent, pathMatch: 'full'}
-        ]),
-        BrowserAnimationsModule,
-        MatStepperModule,
-        MatIconModule,
-        FormsModule,
-        ReactiveFormsModule,
-        MatFormFieldModule,
-        MatButtonModule,
-        MatSliderModule,
-        MatInputModule,
-        MatRippleModule,
-        MatGridListModule,
-        MatListModule,
-        MatSlideToggleModule,
-        MatCardModule,
-        MatAutocompleteModule,
-        MatCheckboxModule,
-        MatRadioModule
-    ],
+  imports: [
+    BrowserModule.withServerTransition({appId: 'ng-cli-universal'}),
+    HttpClientModule,
+    FormsModule,
+    RouterModule.forRoot([
+      {path: '', component: HomeComponent, pathMatch: 'full'},
+      {path: 'sign-in', component: SignInComponent, pathMatch: 'full'},
+      {path: 'on-boarding', component: OnBoardingComponent, pathMatch: 'full'},
+      {path: 'sign-up', component: SignUpComponent, pathMatch: 'full'},
+      {path: 'forget-password', component: ForgetPasswordComponent, pathMatch: 'full'},
+      {path: 'profile-settings', component: ProfileSettingsComponent, pathMatch: 'full'}
+    ]),
+    BrowserAnimationsModule,
+    MatStepperModule,
+    MatIconModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatButtonModule,
+    MatSliderModule,
+    MatInputModule,
+    MatRippleModule,
+    MatGridListModule,
+    MatListModule,
+    MatSlideToggleModule,
+    MatCardModule,
+    MatAutocompleteModule,
+    MatCheckboxModule,
+    MatRadioModule
+
+  ],
   providers: [
     SignInService,
     SignUpService,
@@ -82,4 +83,5 @@ import {SignUpService} from "./Services/sign-up.service";
   ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
