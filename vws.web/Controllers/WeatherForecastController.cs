49555b052149fc0f508ed3fcf283d11d95c6bd65
M.Hassan Shahrian <mh.shahir.7899@icloud.com>
2021-01-02 17:42:37 +0000

    Updated WeatherForecastController.cs

--- vws.web/Controllers/WeatherForecastController.cs
+++ vws.web/Controllers/WeatherForecastController.cs
@@ -36,5 +36,18 @@ namespace vws.web.Controllers
             })
             .ToArray();
         }
+
+        [HttpGet]
+        public IEnumerable<WeatherForecast> Get2()
+        {
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
     }
 }
