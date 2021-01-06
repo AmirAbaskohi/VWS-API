a28deb0cc8194c9f3ebd50872639056269d88492
Mohammad Hassan Shahrian <masan@Mohammads-iMac.local>
2021-01-06 20:14:06 +0330

    Added task.

---
+++ vws.web/Domain/task/TaskDbContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace vws.web.Domain.task
+{
+    public class TaskDbContext : DbContext
+    {
+        public TaskDbContext(DbContextOptions<TaskDbContext> dbContextOptions)
+        : base(dbContextOptions)
+        {
+
+        }
+
+        public DbSet<GeneralTask> Messages { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+    }
+}
