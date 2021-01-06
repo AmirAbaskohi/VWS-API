f6b92990cd65dd387c40703bb6b15f16cf51c4af
Mohammad Hassan Shahrian <masan@Mohammads-iMac.local>
2021-01-06 20:13:31 +0330

    Added chat

---
+++ vws.web/Domain/chat/MessageDbContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace vws.web.Domain.chat
+{
+    public class MessageDbContext : DbContext
+    {
+        public MessageDbContext(DbContextOptions<MessageDbContext> dbContextOptions)
+        : base(dbContextOptions)
+        {
+
+        }
+
+        public DbSet<Message> Messages { get; set; }
+
+        public DbSet<MessageType> MessageTypes { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+    }
+}
