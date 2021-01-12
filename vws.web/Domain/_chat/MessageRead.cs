dd56b6a0056dcd824016c684fe952ec6ebb0777d
Mohammad Hassan Shahrian <masan@Mohammads-iMac.local>
2021-01-12 14:42:28 +0330

    Added MessageRead

---
+++ vws.web/Domain/_chat/MessageRead.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace vws.web.Domain._chat
+{
+    public class MessageRead
+    {
+        public MessageRead()
+        {
+        }
+
+        [Key]
+        public long Id { get; set; }
+
+        public long MessageId { get; set; }
+
+        [MaxLength(256)]
+        public string WhoReadUserName { get; set; }
+
+        public virtual Message Message { get; set; }
+
+    }
+}
