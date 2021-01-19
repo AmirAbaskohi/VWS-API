ea8735acf68c80bd105bde067e82cd492ee16700
AmirHosseinMissMe <amirhossein2324236@gmail.com>
2021-01-19 18:23:11 +0330

    Updated team invite link table.

---
+++ vws.web/Migrations/20210119144515_UpdatedTeamInviteLink.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace vws.web.Migrations
+{
+    public partial class UpdatedTeamInviteLink : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "LinkGuid",
+                table: "Team_TeamInviteLink",
+                type: "uniqueidentifier",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "LinkGuid",
+                table: "Team_TeamInviteLink");
+        }
+    }
+}
