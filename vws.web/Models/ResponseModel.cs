3174ffc4e1f82507f4d0e4faad95d7559faa1844
AmirHosseinMissMe <amirhossein2324236@gmail.com>
2021-01-19 14:34:45 +0330

    Added generic response model.

--- vws.web/Models/ResponseModel.cs
+++ vws.web/Models/ResponseModel.cs
@@ -12,9 +12,25 @@ namespace vws.web.Models
             Errors = new List<string>();
         }
         public void AddError(string error) => Errors.Add(error);
+        public void AddErrors(IEnumerable<string> errors) => Errors.AddRange(errors);
         public string Status { get => Errors.Count == 0 ? "Success" : "Error"; }
         public string Message { get; set; }
         public bool HasError { get => Errors.Count > 0;}
         public List<string> Errors { get; set; }
+    }
+
+    public class ResponseModel<T> : ResponseModel
+    {
+        public T Value { get; set; }
+        public ResponseModel() { }
+        public ResponseModel(T value)
+        {
+            Value = value;
+        }
+        public ResponseModel(T value, List<string> errors)
+        {
+            Value = value;
+            AddErrors(errors);
+        }
     }
 }
