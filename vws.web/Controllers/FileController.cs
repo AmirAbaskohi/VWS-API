5c16f8ff5525d52cffceecb02fb57ad81d292568
AmirHosseinMissMe <amirhossein2324236@gmail.com>
2021-01-21 16:50:09 +0330

    Added file upload api.

---
+++ vws.web/Controllers/FileController.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Localization;
+using vws.web.Domain;
+
+namespace vws.web.Controllers
+{
+    [Route("{culture:culture}/[controller]")]
+    [ApiController]
+    public class FileController : BaseController
+    {
+        private readonly IConfiguration configuration;
+        private readonly IStringLocalizer<TeamController> localizer;
+        private readonly IVWS_DbContext vwsDbContext;
+
+        public FileController(IConfiguration _configuration, IStringLocalizer<TeamController> _localizer,
+            IVWS_DbContext _vwsDbContext)
+        {
+            configuration = _configuration;
+            localizer = _localizer;
+            vwsDbContext = _vwsDbContext;
+        }
+
+        private async Task<bool> WriteFile(IFormFile file)
+        {
+            bool isSaveSuccess = false;
+            string fileName;
+            try
+            {
+                var extension = "." + file.FileName.Split('.')[file.FileName.Split('.').Length - 1];
+                fileName = Guid.NewGuid().ToString() + extension;
+
+                var pathBuilt = Path.Combine(Directory.GetCurrentDirectory(), "Upload\\files");
+
+                if (!Directory.Exists(pathBuilt))
+                {
+                    Directory.CreateDirectory(pathBuilt);
+                }
+
+                var path = Path.Combine(Directory.GetCurrentDirectory(), "Upload\\files",
+                   fileName);
+
+                using (var stream = new FileStream(path, FileMode.Create))
+                {
+                    await file.CopyToAsync(stream);
+                }
+
+                isSaveSuccess = true;
+            }
+            catch (Exception e)
+            {
+                //log error
+            }
+
+            return isSaveSuccess;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> UploadFiles(IFormFile file)
+        {
+            if (await WriteFile(file))
+                return Ok();
+            return StatusCode(StatusCodes.Status500InternalServerError);
+        }
+    }
+}
