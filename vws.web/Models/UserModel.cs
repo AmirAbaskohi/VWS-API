9002ff1a7b89a9e00999cc6cf3d131effb6aa720
TheRealRondon <amirhossein2324236@gmail.com>
2021-03-10 17:09:58 +0330

    Solved distinct problem in get project users.

--- vws.web/Models/UserModel.cs
+++ vws.web/Models/UserModel.cs
@@ -5,10 +5,23 @@ using System.Threading.Tasks;
 
 namespace vws.web.Models
 {
-    public class UserModel
+    public class UserModel : IEquatable<UserModel>
     {
         public string UserName { get; set; }
         public Guid UserId { get; set; }
         public Guid? ProfileImageGuid { get; set; }
+
+        public bool Equals(UserModel other)
+        {
+            if (UserId == other.UserId)
+                return true;
+
+            return false;
+        }
+
+        public override int GetHashCode()
+        {
+            return UserId.GetHashCode();
+        }
     }
 }
