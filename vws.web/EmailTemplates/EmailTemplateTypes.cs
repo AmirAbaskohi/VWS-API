1b8d0202ee20b374348d4f04268211d1a85ed7f1
masan <mh.shahir.7899@icloud.com>
2021-03-21 12:02:38 +0330

    * SendEmail.docx: Added SendEmail doc.
    * EmailTemplateTypes.cs:
    * EmailTemplateUtility.cs: Added EmailUtility & EmailType.

---
+++ vws.web/EmailTemplates/EmailTemplateTypes.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace vws.web.EmailTemplates
+{
+    public class EmailTemplateTypes
+    {
+        public enum EmailTemplateEnum : int
+        {
+            EmailVerificationCode = 1,
+            ResetPassword = 2,
+            ChangePasswordAlert = 3
+        };
+    }
+}
---
+++ vws.web/EmailTemplates/EmailTemplateUtility.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+using static vws.web.EmailTemplates.EmailTemplateTypes;
+
+namespace vws.web.EmailTemplates
+{
+    public static class EmailTemplateUtility
+    {
+        public static string GetEmailTemplate(int inputType)
+        {
+            var types = Enum.GetValues(typeof(EmailTemplateEnum));
+            string path = Path.Combine(Directory.GetCurrentDirectory(), $"EmailTemplates{Path.DirectorySeparatorChar}");
+
+            foreach (var type in types)
+            {
+                if ((int)type == inputType)
+                {
+                    path += type.ToString();
+                    break;
+                }
+            }
+            path += ".html";
+            string content = File.ReadAllText(path);
+            return content;
+        }
+
+
+    }
+}
